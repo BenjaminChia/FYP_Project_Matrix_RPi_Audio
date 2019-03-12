a90dfe092e3dec7b61ed54140410b644aaad598a
BenjaminChia <ben9694@gmail.com>
2019-03-12 19:39:50 +0800

    Experimenting with recorder

--- main/main.cpp
+++ main/main.cpp
@@ -8,8 +8,8 @@
 #include <string.h>
 #include <wiringPi.h>
 #include <pthread.h>
-//#include <unistd.h>
-
+#include <unistd.h>
+#include <sstream>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -24,16 +24,25 @@
 #include "../matrix/cpp/driver/microphone_core.h"
 
 //#include "../SharedResources.cpp"
+#define BUFFER_SAMPLES_PER_CHANNEL	16000 //1 second of recording
+#define STREAMING_CHANNELS			8 //Maxmium 8 channels
+const int32_t bufferByteSize = STREAMING_CHANNELS * BUFFER_SAMPLES_PER_CHANNEL * sizeof(int16_t);
+
+#define HOST_NAME_LENGTH			20 //maximum number of characters for host name
+#define COMMAND_LENGTH				1
 
 DEFINE_int32(sampling_frequency, 16000, "Sampling Frequency");
 DEFINE_int32(duration, 10, "Interrupt after N seconds");
 DEFINE_int32(gain, 5, "Microphone Gain");
 DEFINE_bool(big_menu, true, "Include 'advanced' options in the menu listing");
-
+using namespace std;
 namespace hal = matrix_hal;
 
 int led_offset[] = { 23, 27, 32, 1, 6, 10, 14, 19 };
 int lut[] = { 1, 2, 10, 200, 10, 2, 1 };
+char sysInfo[COMMAND_LENGTH + HOST_NAME_LENGTH];
+
+char* hostname = &sysInfo[1];
 
 int main(int argc, char *agrv[]) {
 	google::ParseCommandLineFlags(&argc, &agrv, true);
@@ -78,18 +87,68 @@ int main(int argc, char *agrv[]) {
 
 	mics.CalculateDelays(0, 0, 1000, 320 * 1000);
 
-	std::ofstream os[mics.Channels() + 1];
+	ofstream os[mics.Channels() + 1];
+	uint32_t bufferSwitch = 0;
+	char dateAndTime[16];
+	struct tm tm;
+	
+	time_t t = time(NULL);
+	tm = *localtime(&t);
+	gethostname(hostname, HOST_NAME_LENGTH);
+	sprintf(dateAndTime, "%d%02d%02d_%02d%02d%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+		tm.tm_hour, tm.tm_min, tm.tm_sec);
+	ostringstream filenameStream;
+	filenameStream << "/home/pi/Recordings/" << hostname << "_" << dateAndTime << "_8ch.wav";
+	string filename = filenameStream.str();
+	cout << filename << endl;
 
-	for (uint16_t c = 0; c < mics.Channels() + 1; c++) {
-		std::string filename = "mic_" + std::to_string(mics.SamplingRate()) +
+	ofstream file(filename, std::ofstream::binary);
+	struct WaveHeader {
+		//RIFF chunk
+		char RIFF[4] = { 'R', 'I', 'F', 'F' };
+		uint32_t overallSize;						// overall size of file in bytes
+		char WAVE[4] = { 'W', 'A', 'V', 'E' };		// WAVE string
+
+													//fmt subchunk
+		char fmt[4] = { 'f', 'm', 't', ' ' };		// fmt string with trailing null char
+		uint32_t fmtLength = 16;					// length of the format data
+		uint16_t audioFormat = 1;					// format type. 1-PCM, 3- IEEE float, 6 - 8bit A law, 7 - 8bit mu law
+		uint16_t numChannels = 8;					// no.of channels
+		uint32_t samplingRate = 16000;				// sampling rate (blocks per second)
+		uint32_t byteRate = 256000;					// SampleRate * NumChannels * BitsPerSample/8
+		uint16_t blockAlign = 16;					// NumChannels * BitsPerSample/8
+		uint16_t bitsPerSample = 16;				// bits per sample, 8- 8bits, 16- 16 bits etc
+
+													//data subchunk
+		char data[4] = { 'd', 'a', 't', 'a' };		// DATA string or FLLR string
+		uint32_t dataSize;							// NumSamples * NumChannels * BitsPerSample/8 - size of the next chunk that will be read
+	} header;
+
+	
+	header.dataSize = bufferByteSize ;
+	header.overallSize = header.dataSize + 36;
+	file.seekp(0);
+	file.write((const char*)&header, sizeof(WaveHeader));
+	file.close();
+
+	for (uint16_t c = 8; c < mics.Channels() + 1; c++) {
+		string filename = "mic_" + std::to_string(mics.SamplingRate()) +
 			"_s16le_channel_" + std::to_string(c) + ".raw";
-		os[c].open(filename, std::ofstream::binary);
+		os[c].open(filename, ofstream::binary);
 	}
 	
 
 	//DOA lights
 	uint32_t samples = 0;
 	//record + DOA
+	try
+	{
+
+	}
+	catch (const std::exception&)
+	{
+			
+	}
 	for (int s = 0; s < seconds_to_record; s++) {
 		for (;;) {
 			mics.Read(); /* Reading 8-mics buffer from de FPGA */
@@ -102,7 +161,7 @@ int main(int argc, char *agrv[]) {
 				buffer[mics.Channels()][samples] = mics.Beam(s);
 				samples++;
 			}//recording ends
-
+			//file.write((const char*)buffer[bufferSwitch], bufferByteSize);
 			//DOA part
 			doa.Calculate();
 			mic = doa.GetNearestMicrophone();
@@ -123,7 +182,7 @@ int main(int argc, char *agrv[]) {
 
 			//write to file
 			if (samples >= mics.SamplingRate()) {
-				for (uint16_t c = 0; c < mics.Channels() + 1; c++) {
+				for (uint16_t c = 8; c < mics.Channels() + 1; c++) {
 					os[c].write((const char *)buffer[c], samples * sizeof(int16_t));
 				}
 				samples = 0;
@@ -131,6 +190,8 @@ int main(int argc, char *agrv[]) {
 			}
 		}
 	}
+	/*
+	*/
 	return 0;
 }
 
